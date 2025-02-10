d9a8502523416f42125a39c9397abbc53a674b34
Toto <tobiasarraiza17@gmail.com>
2025-02-10 16:55:01 -0300

    falta ponerlo lindoooooo

--- typing.js
+++ typing.js
@@ -1,6 +1,6 @@
 let words = 'in one good real one not school set they state high life consider on and not come what also for set point can want as while with of order child about school thing never hold find order each too between program work end you home place around while place problem end begin interest while public or where see time those increase interest be give end think seem small as both another a child same eye you between way do who into again good fact than under very head become real possible some write know however late each that with because that place nation only for each change form consider we would interest with world so order or run more open that large write turn never over open each over change still old take hold need give by consider line only leave while what set up number part form want against great problem can because head so first this here would course become help year first end want both fact public long word down also long for without new turn against the because write seem line interest call not if line thing what work people way may old consider leave hold want life between most place may if go who need fact such program where which end off child down change to from people high during people find to however into small new general it do that could old for last get another hand much eye great no work and with but good there last think can around use like number never since world need what we around part show new come seem while some and since still small these you general which seem will place come order form how about just also they with state late use both early too lead general seem there point take general seem few out like might under if ask while such interest feel word right again how about system such between late want fact up problem stand new say move a lead small however large public out by eye here over so be way use like say people work for since interest so face order school good not most run problem group run she late other problem real form what just high no man do under would to each too end point give number child through so this large see get form also all those course to work during about he plan still so like down he look down where course at who plan way so since come against he all who at world because while so few last these mean take house who old way large no first too now off would in this course present order home public school back own little about he develop of do over help day house stand present another by few come that down last or use say take would each even govern play around back under some line think she even when from do real problem between long as there school do as mean to all on other good may from might call world thing life turn of he look last problem after get show want need thing old other during be again develop come from consider the now number say life interest to system only group world same state school one problem between for turn run at very against eye must go both still all a as so after play eye little be those should out after which these both much house become both school this he real and may mean time by real number other as feel at end ask plan come turn by all head increase he present increase use stand after see order lead than system here ask in of look point little too without each for both but right we come world much own set we right off long those stand go both but under now must real general then before with much those at no of we only back these person plan from run new as own take early just increase only look open follow get that on system the mean plan man over it possible if most late line would first without real hand say turn point small set at in system however to be home show new again come under because about show face child know person large program how over could thing from out world while nation stand part run have look what many system order some one program you great could write day do he any also where child late face eye run still again on by as call high the must by late little mean never another seem to leave because for day against public long number word about after much need open change also'.split(' '); /*Esto hace que divida el STRING en un array*/
 let wordsCount = words.length;
-const gameTime = 10 * 1000; //Tiempo de juego
+const gameTime = 30 * 1000; //Tiempo de juego
 window.timer = null;
 window.gameStart = null;
 window.pauseTime = 0;
@@ -40,7 +40,7 @@ function changeLanguage(newWords) {
 
 
 /*PUNTAJES WPM*/
-let scoreCount = 0;
+let scoreCount = 0; //Suma de los intentos
 
 function updateScoreboard(wpm) {
     scoreCount++;
@@ -76,12 +76,12 @@ function removeClass(el, name) {
 /*FUNCION PARA DAR PALABRA RANDOM */
 function randomWord() {
   const randomIndex = Math.ceil(Math.random() * wordsCount); //.ceil redondea para arriba
-  return words[randomIndex - 1];
+  return words[randomIndex - 1]; //Retorna la palabra al final del array 
 }
 
 /*FORMATO DE LA PALABRA */ 
 function formatWord(word) {
-  return `<div class="word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`;
+  return `<div class="word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`; //Split divide el string en un array y join lo une con un string 
 }
 
 
@@ -103,24 +103,24 @@ function newGame() {
   // Reiniciar estado
   addClass(document.querySelector('.word'), 'current');
   addClass(document.querySelector('.letter'), 'current');
-  document.getElementById('info').innerHTML = (gameTime / 1000) + '';
+  document.getElementById('info').innerHTML = (gameTime / 1000) + ''; // Convertir segundos a minutos y segundos para mostrar en la interfaz 
 
   // Reiniciar variables globales
-  clearInterval(window.timer);
-  window.timer = null;
-  window.gameStart = null;
-  window.pauseTime = 0;
+  clearInterval(window.timer); // Detener el temporizador
+  window.timer = null; // Reiniciar el temporizador
+  window.gameStart = null; 
+  window.pauseTime = 0; // Reiniciar el tiempo de pausa 
 
 }
 
 function newGameWPM() {
     console.log('Juego reseteado y listo para empezar en modo WPM');
     
-    // Eliminar la clase "over" antes de reiniciar
-    removeClass(document.getElementById('game'), 'over');
+    // Eliminar la clase "over" antes de reiniciar 
+    removeClass(document.getElementById('game'), 'over'); 
     
     // Reiniciar estado
-    const wordsContainer = document.getElementById('words');
+    const wordsContainer = document.getElementById('words'); 
     wordsContainer.style.marginTop = '0px';
     addClass(document.querySelector('.word'), 'current');
     addClass(document.querySelector('.letter'), 'current');
@@ -135,26 +135,26 @@ function newGameWPM() {
 
 
 function getWpm() {
-  const words = [...document.querySelectorAll('.word')];
-  const lastTypedWord = document.querySelector('.word.current');
-  const lastTypedWordIndex = words.indexOf(lastTypedWord) + 1;
-  const typedWords = words.slice(0, lastTypedWordIndex);
-  const correctWords = typedWords.filter(word => {
-    const letters = [...word.children];
-    const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect'));
-    const correctLetters = letters.filter(letter => letter.className.includes('correct'));
-    return incorrectLetters.length === 0 && correctLetters.length === letters.length;
+  const words = [...document.querySelectorAll('.word')]; // Obtener todas las palabras
+  const lastTypedWord = document.querySelector('.word.current'); // Obtener la última palabra escrita
+  const lastTypedWordIndex = words.indexOf(lastTypedWord) + 1; // Obtener el índice de la última palabra escrita
+  const typedWords = words.slice(0, lastTypedWordIndex); // Obtener todas las palabras escritas hasta la última palabra escrita 
+  const correctWords = typedWords.filter(word => { 
+    const letters = [...word.children]; 
+    const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect')); // Obtener todas las letras incorrectas
+    const correctLetters = letters.filter(letter => letter.className.includes('correct'));  // Obtener todas las letras correctas
+    return incorrectLetters.length === 0 && correctLetters.length === letters.length; // Verificar si todas las letras son correctas 
   });
-  return correctWords.length / gameTime * 60000;
+  return correctWords.length / gameTime * 60000; // Calcular las palabras por minuto (WPM)
 }
 
 
 /************************************************************************ */
 function gameOver() {
-  clearInterval(window.timer);
-  addClass(document.getElementById('game'), 'over');
-  const result = getWpm();
-  document.getElementById('info').innerHTML = `WPM: ${result.toFixed(2)}`;
+  clearInterval(window.timer); // Detener el temporizador
+  addClass(document.getElementById('game'), 'over'); // Agregar la clase "over" al div game
+  const result = getWpm(); // Calcular el WPM
+  document.getElementById('info').innerHTML = `WPM: ${result.toFixed(2)}`; // Mostrar el WPM en el div info 
   
   // Ocultar el div game y mostrar la pantalla de fin de juego en su lugar
   const gameDiv = document.getElementById('game');
@@ -169,7 +169,7 @@ function gameOver() {
   gameOverScreen.style.width = gameDiv.offsetWidth + 'px';
   gameOverScreen.style.height = gameDiv.offsetHeight + 'px';
   
-  addClass(gameDiv, 'hidden');
+  addClass(gameDiv, 'hidden'); 
   document.getElementById('final-wpm').innerHTML = `WPM: ${result.toFixed(2)}`;
   removeClass(gameOverScreen, 'hidden');
   addClass(gameOverScreen, 'visible');
@@ -181,13 +181,13 @@ function gameOver() {
   scoreboard.style.display = 'block';
   
   // Actualizar la tabla de puntajes
-  updateScoreboard(result);
+  updateScoreboard(result); 
 }
 
 document.getElementById('newGameBtn').addEventListener('click', () => {
-  clearInterval(window.timer);
+  clearInterval(window.timer); // Detener el temporizador
   window.timer = null;
-  newGame();
+  newGame(); // Reiniciar el juego
 });
 
 // Añadir evento para el botón de reiniciar
@@ -217,13 +217,12 @@ document.getElementById('newGameBtn').addEventListener('click', function() {
     clearInterval(window.timer);
     window.timer = null;
     newGameWPM();
-    // ...código adicional para iniciar el modo WPM...
 });
 
 document.getElementById('levelModeBtn').addEventListener('click', function() {
     this.classList.add('active');
     document.getElementById('newGameBtn').classList.remove('active');
-    // ...código adicional para iniciar el modo de nivel...
+    startLevelMode();
 });
 
 /************************************************************************ */
@@ -251,8 +250,8 @@ document.getElementById('game').addEventListener('keyup', ev => {
       if (!window.gameStart) {
         window.gameStart = (new Date()).getTime();
       }
-      const currentTime = (new Date()).getTime();
-      const msPassed = currentTime - window.gameStart;
+      const currentTimeFF = (new Date()).getTime();
+      const msPassed = currentTimeFF - window.gameStart;
       const sPassed = Math.round(msPassed / 1000);
       const sLeft = Math.round((gameTime / 1000) - sPassed);
       if (sLeft <= 0) {
@@ -329,13 +328,6 @@ document.getElementById('game').addEventListener('keyup', ev => {
     words.style.marginTop = (margin - 35) + 'px';
   }
 
-  // move cursor
-  /*
-  const nextLetter = document.querySelector('.letter.current');
-  const nextWord = document.querySelector('.word.current');
-  const cursor = document.getElementById('cursor');
-  cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 2 + 'px';
-  cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 'px';*/
 });
 
 document.getElementById('newGameBtn').addEventListener('click', () => {
@@ -349,12 +341,15 @@ newGame();
 
 
 /*NIVEL DE JUEGO */
+/*
 let level = 1;
 let levelScore = 0;
 let levelTime = 15000; // Tiempo inicial en milisegundos
+// */
 let cantidadPalabrasPorJuego = 10;
 
 function startLevelMode() {
+    console.log("se ejecuta la funcion startLevelMode");
     level = 1;
     levelScore = 0;
     levelTime = 15000; // Reiniciar el tiempo inicial
@@ -362,56 +357,64 @@ function startLevelMode() {
 }
 
 function startNewLevel() {
-    const wordsContainer = document.getElementById('words');
-    wordsContainer.innerHTML = '';
-    wordsContainer.style.marginTop = '0px';
+  console.log("se ejecuta la funcion start New Level");
+  const wordsContainer = document.getElementById('words');
+  wordsContainer.innerHTML = '';
+  wordsContainer.style.marginTop = '0px';
 
-    // Generar nuevas palabras
-    for (let i = 0; i < cantidadPalabrasPorJuego; i++) {
-        wordsContainer.innerHTML += formatWord(randomWord());
+  // Generar nuevas palabras
+  for (let i = 0; i < cantidadPalabrasPorJuego; i++) {
+    wordsContainer.innerHTML += formatWord(randomWord());
+  }
+
+  addClass(document.querySelector('.word'), 'current');
+  addClass(document.querySelector('.letter'), 'current');
+  document.getElementById('info').innerHTML = `Nivel: ${level} - Tiempo: ${(levelTime / 1000)}`;
+  clearInterval(window.timer);
+
+  window.timer = setInterval(() => {
+    if (!window.gameStart) {
+      window.gameStart = (new Date()).getTime();
     }
-
-    addClass(document.querySelector('.word'), 'current');
-    addClass(document.querySelector('.letter'), 'current');
-    document.getElementById('info').innerHTML = (levelTime / 1000) + '';
-    clearInterval(window.timer);
-    
-    window.timer = setInterval(() => {
-        if (!window.gameStart) {
-            window.gameStart = (new Date()).getTime();
-        }
-        const currentTime = (new Date()).getTime();
-        const msPassed = currentTime - window.gameStart;
-        const sPassed = Math.round(msPassed / 1000);
-        const sLeft = Math.round((levelTime / 1000) - sPassed);
-        if (sLeft <= 0) {
-            gameOverLevelMode();
-            return;
-        }
-        document.getElementById('info').innerHTML = sLeft + '';
-    }, 1000);
-}
-
-function levelUp() {
-    if (checkLevelCompletion()) {
-      console.log("Se subio el lvl")
-        levelScore += 100 + level; // Sumar 100 puntos más el nivel actual
-        level++; // Incrementar el nivel
-        console.log("Nivel actual:" + level)
-        const currentTime = (new Date()).getTime();
-        const msPassed = currentTime - window.gameStart;
-        const sPassed = Math.round(msPassed / 1000);
-        levelTime = levelTime-1; // Reducir el tiempo en 3 segundos por nivel, mínimo 5 segundos
-        startNextLevel(); // Iniciar el siguiente nivel
+    const currentTime = (new Date()).getTime();
+    const msElapsed = currentTime - window.gameStart;
+    const secondsElapsed = Math.round(msElapsed / 1000);
+    const secondsRemaining = Math.max(Math.round((levelTime / 1000) - secondsElapsed), 0); // Asegurarse de que no sea negativo
+    document.getElementById('info').innerHTML = `Nivel: ${level} - Tiempo: ${secondsRemaining}`;
+    if (secondsRemaining <= 0) {
+      console.log("secondsRemaining: " + secondsRemaining);
+      console.log("LevelTime: " + levelTime);
+      console.log("secondsElapsed: " + secondsElapsed);
+      clearInterval(window.timer); // Asegurarse de detener el temporizador
+      gameOverLevelMode();
+      return;
     }
+  }, 1000);
+
+  // Enfocar el juego para permitir escribir
+  setTimeout(() => {
+    document.getElementById('game').focus();
+  }, 100);
 }
 
 function startNextLevel() {
-    levelTime = levelTime-3; // Reducir el tiempo en 3 segundos por nivel, mínimo 5 segundos
-    startNewLevel();
+  levelTime = Math.max(levelTime - 1000, 5000); // Reducir el tiempo en 3 segundos por nivel, mínimo 5 segundos 
+  window.gameStart = null; // Reiniciar el tiempo de inicio del juego
+  startNewLevel();
+}
+
+function levelUp() {
+  if (checkLevelCompletion()) {
+    console.log("Se subio el lvl");
+    levelScore += 100 + level; // Sumar 100 puntos más el nivel actual
+    level++; // Incrementar el nivel
+    console.log("Nivel actual:" + level);
+    startNextLevel(); // Iniciar el siguiente nivel
+  }
 }
 
 function gameOverLevelMode() {
+    console.log("Se ejecuta la funcion gameOverLevelMode")
     clearInterval(window.timer);
     addClass(document.getElementById('game'), 'over');
     document.getElementById('info').innerHTML = `Score: ${levelScore}`;
@@ -445,9 +448,6 @@ document.getElementById('levelModeBtn').addEventListener('click', function() {
     startLevelMode();
 });
 
-document.getElementById('next-level-button').removeEventListener('click', startNextLevelHandler);
-document.getElementById('next-level-button').addEventListener('click', startNextLevelHandler);
-
 function startNextLevelHandler() {
     const levelEndScreen = document.getElementById('level-end');
     const gameDiv = document.getElementById('game');
@@ -470,10 +470,16 @@ function startNextLevelHandler() {
     startNextLevel();
 }
 
+document.getElementById('next-level-button').removeEventListener('click', startNextLevelHandler);
+document.getElementById('next-level-button').addEventListener('click', startNextLevelHandler);
+
+// Pase de nivel si se presiona el espacio
 document.getElementById('game').addEventListener('keyup', ev => {
     const isSpace = ev.code === 'Space';
     if (isSpace) {
         if (document.querySelector('.word.current') === null) {
+            levelUp();
+        } else if (checkLevelCompletion()) {
             levelUp();
         }
     }
@@ -490,12 +496,12 @@ function checkLevelCompletion() {
             }
         }
     }
-    console.log("todo completo")
+    console.log("todo completo");
     return true;
 }
 
-
 document.getElementById('next-level-button').addEventListener('click', function() {
+    console.log("clickeo boton next-level");
     const levelEndScreen = document.getElementById('level-end');
     const gameDiv = document.getElementById('game');
     const header = document.getElementById('header');
@@ -508,49 +514,13 @@ document.getElementById('next-level-button').addEventListener('click', function(
 
     // Mostrar el juego
     removeClass(gameDiv, 'hidden');
-
     // Mostrar el header y los botones de idioma
     header.style.display = 'block';
     languageButtons.style.display = 'flex';
+    newGame();
+    startLevelMode();
 
-    // Iniciar el siguiente nivel
-    startNextLevel();
+    // Enfocar el juego para permitir escribir
+    gameDiv.focus();
 });
-
-function startNextLevel() {
-    levelTime = levelTime-3; // Reducir el tiempo en 3 segundos por nivel, mínimo 5 segundos
-    startNewLevel();
-}
-
-function startNewLevel() {
-    const wordsContainer = document.getElementById('words');
-    wordsContainer.innerHTML = '';
-    wordsContainer.style.marginTop = '0px';
-
-    // Generar nuevas palabras
-    for (let i = 0; i < cantidadPalabrasPorJuego; i++) {
-        wordsContainer.innerHTML += formatWord(randomWord());
-    }
-
-    addClass(document.querySelector('.word'), 'current');
-    addClass(document.querySelector('.letter'), 'current');
-    document.getElementById('info').innerHTML = (levelTime / 1000) + '';
-    clearInterval(window.timer);
-    
-    window.timer = setInterval(() => {
-        if (!window.gameStart) {
-            window.gameStart = (new Date()).getTime();
-        }
-        const currentTime = (new Date()).getTime();
-        const msPassed = currentTime - window.gameStart;
-        const sPassed = Math.round(msPassed / 1000);
-        const sLeft = Math.round((levelTime / 1000) - sPassed);
-        if (sLeft <= 0) {
-            gameOverLevelMode();
-            return;
-        }
-        document.getElementById('info').innerHTML = sLeft + '';
-    }, 1000);
-}
-
-// Elimina el listener duplicado
+ 
